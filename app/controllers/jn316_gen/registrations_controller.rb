d17667284246e249189e2e685d45ec11b71c2713
Vladimir Tamara <vtamara@pasosdeJesus.org>
2017-08-31 14:14:09 -0500

    Sin errores tras cambiar clave correctamente

--- app/controllers/jn316_gen/registrations_controller.rb
+++ app/controllers/jn316_gen/registrations_controller.rb
@@ -5,6 +5,10 @@ require 'devise/registrations_controller'
 class Jn316Gen::RegistrationsController < ::Devise::RegistrationsController
   include Jn316Gen::LdapHelper
 
+  def after_update_path_for(resource)
+    Rails.configuration.relative_url_root
+  end
+
   def update
     if params[:usuario] && params[:usuario][:password] &&
       params[:usuario][:password_confirmation] &&
@@ -16,17 +20,30 @@ class Jn316Gen::RegistrationsController < ::Devise::RegistrationsController
                            params[:usuario][:current_password],
                            params[:usuario][:password], prob)
         flash[:notice] = 'Clave cambiada en directorio LDAP'
+        puts "Jn316Gen::RegistrationsController#update si cambio clave en ldap"
+        # Para salir super usará after_update_path_for(resource)
+        super
+        puts "Jn316Gen::RegistrationsController#update paso super"
+         #redirect_to Rails.configuration.relative_url_root
+        return
       else
+        puts "Jn316Gen::RegistrationsController#update no cambió clave en ldap prob=#{prob}"
         flash[:error] = 'No pudo cambiar clave en directorio LDAP: ' + prob
         redirect_to Rails.configuration.relative_url_root
         return
       end
+      puts "Jn316Gen::RegistrationsController#update no deberia llegar aqui"
     end
-    super
+    flash[:error] = 'Claves incorrectas'
+    redirect_to request.referrer
+    puts "Jn316Gen::RegistrationsController#update no deberia llegar aqui"
+    return
   rescue Exception => exception
     flash[:error] = 'No pudo cambiar clave en directorio LDAP: ' + 
       exception.to_s
+    puts "Jn316Gen::RegistrationsController#update excepcion #{exception.to_s}"
     redirect_to Rails.configuration.relative_url_root
+    return
   end
 
 end
