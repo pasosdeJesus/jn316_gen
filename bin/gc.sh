647f18605b6c58c2cd4cf7e79cf771797d5918d1
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2019-06-09 10:46:01 -0500

    Actualiza

--- bin/gc.sh
+++ bin/gc.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
-# Hace pruebas, pruebas de regresión y envia a github
+# Revisa errore comunes, ejecuta pruebas de regresión y del sistema y envia a github 
 
-grep "^ *gem.*sip.*,.*path:" Gemfile > /dev/null 2> /dev/null
+function cableado {
+	for n in $*; do 
+		echo "Revisando $n"
+		grep "^ *gem *.${n}.*, *path:" Gemfile > /dev/null 2> /dev/null
+		if (test "$?" = "0") then {
+			echo "Gemfile incluye un ${n} cableado al sistema de archivos"
+			exit 1;
+		} fi;
+	done
+}
+
+grep "^ *gem *.debugger*" Gemfile > /dev/null 2> /dev/null
 if (test "$?" = "0") then {
-	echo "Gemfile incluye un sip cableado al sistema de archivos"
+	echo "Gemfile incluye debugger"
 	exit 1;
 } fi;
-grep "^ *gem.*debugger.*" Gemfile > /dev/null 2> /dev/null
-if (test "$?" = "0") then {
-	echo "Gemfile incluye debugger que heroku no quiere"
-	exit 1;
-} fi;
-grep "^ *gem.*byebug.*" Gemfile > /dev/null 2> /dev/null
+grep "^ *gem *.byebug*" Gemfile > /dev/null 2> /dev/null
 if (test "$?" = "0") then {
 	echo "Gemfile incluye byebug que rbx de travis-ci no quiere"
 	exit 1;
@@ -22,13 +28,19 @@ if (test "$SINAC" != "1") then {
 	if (test "$?" != "0") then {
 		exit 1;
 	} fi;
+	CXX=c++ yarn upgrade
+	if (test "$?" != "0") then {
+		exit 1;
+	} fi;
 } fi;
+
 if (test "$SININS" != "1") then {
 	NOKOGIRI_USE_SYSTEM_LIBRARIES=1 MAKE=gmake make=gmake QMAKE=qmake4 bundle install
 	if (test "$?" != "0") then {
 		exit 1;
 	} fi;
 } fi;
+
 if (test "$SINMIG" != "1") then {
 	(cd test/dummy; bin/rails db:migrate sip:indices db:structure:dump)
 	if (test "$?" != "0") then {
@@ -42,22 +54,23 @@ if (test "$?" != "0") then {
 	exit 1;
 } fi;
 
-bin/rails test
+CONFIG_HOSTS=www.example.com bin/rails test
 if (test "$?" != "0") then {
-	echo "No pasaron pruebas";
+	echo "No pasaron pruebas de regresion";
 	exit 1;
 } fi;
 
 (cd test/dummy; RAILS_ENV=test bin/rails db:structure:dump)
+
 b=`git branch | grep "^*" | sed -e  "s/^* //g"`
 git status -s
 if (test "$MENSCONS" = "") then {
-	git commit -a
-} else {
-	git commit -m $MENSCONS -a
+	MENSCONS="Actualiza"
 } fi;
+git commit -m "$MENSCONS" -a
 git push origin ${b}
 if (test "$?" != "0") then {
 	echo "No pudo subirse el cambio a github";
 	exit 1;
 } fi;
+
