fd9825c4f94600ea3d64d6a177a56e2cbbed1872
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-11-23 09:30:22 -0500

    Para actualizar pasar primero a jn316_gen 2.1 y después de ese a 2.2

--- db/migrate/20170328172001_agrega_fecha_sinc.rb
+++
@@ -1,5 +0,0 @@
-class AgregaFechaSinc < ActiveRecord::Migration[5.0]
-  def change
-    add_column :usuario, :ultimasincldap, :date
-  end
-end
--- db/migrate/20170418143627_cn_grupo.rb
+++
@@ -1,5 +0,0 @@
-class CnGrupo < ActiveRecord::Migration[5.0]
-  def change
-    add_column :sip_grupo, :cn, :string, limit: 255
-  end
-end
